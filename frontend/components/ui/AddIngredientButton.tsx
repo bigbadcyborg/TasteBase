037d6f402282f9755b53b75bd717078e3a15506a
Jeremiah Stone <chezwizzproductions@gmail.com>
2025-09-23 17:25:06 -0500

    Created empty button component
    Began implementation for an "Add Ingredient" button within the pantry tab

---
+++ frontend/components/ui/AddIngredientButton.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren, useState } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+type Props = {
+  onPress: () => void;
+};
+
+export function AddIngredientBUtton({ onPress }: Props) {
+
+}
+
+const styles = StyleSheet.create({});
