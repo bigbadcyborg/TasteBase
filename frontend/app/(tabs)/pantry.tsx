98530630ae912c0b275ba951a6cd6e51cd998b5e
Jeremiah Stone <chezwizzproductions@gmail.com>
2025-09-23 17:44:11 -0500

    Added button implementation and added it to the pantry tab

--- frontend/app/(tabs)/pantry.tsx
+++ frontend/app/(tabs)/pantry.tsx
@@ -2,10 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import { ThemedView as ThemedView } from '@/components/themed-view';
 import { ThemedText } from '@/components/themed-text';
+import { AddIngredientBUtton } from '@/components/ui/AddIngredientButton';
 
 export default function TabPantryScreen() {
   return (
     <ThemedView style={styles.container}>
+        <AddIngredientBUtton onPress={() => {}} />
         <ThemedText type="title" style={styles.header}>Pantry</ThemedText>
         <ThemedText style={styles.subtitle}>All your ingredients, at a glance.</ThemedText>
     </ThemedView>
--- frontend/components/ui/AddIngredientButton.tsx
+++ frontend/components/ui/AddIngredientButton.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
@@ -12,7 +13,24 @@ type Props = {
 };
 
 export function AddIngredientBUtton({ onPress }: Props) {
-
+  return (
+    <TouchableOpacity
+      style={[styles.addButton]}
+      onPress={onPress}
+      activeOpacity={0.8}
+    >
+      <AntDesign name="plus" size={32} color="white" />
+    </TouchableOpacity>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  addButton: {
+    position: 'absolute',
+    left: 16,
+    paddingHorizontal: 32,
+    paddingVertical: 32,
+    borderRadius: 16,
+  },
+  addButtonText: { fontSize: 16, fontWeight: '600' },
+});
