876f9d2efa36f72467fb80a13a9cfa45477fe914
Heston-M <riy21@txstate.edu>
2025-09-23 15:03:45 -0500

    EWOK-3 Added title and subtitle
    Added "Pantry" title to pantry tab and a subtitle.

--- frontend/app/(tabs)/pantry.tsx
+++ frontend/app/(tabs)/pantry.tsx
@@ -6,7 +6,8 @@ import { ThemedText } from '@/components/themed-text';
 export default function TabPantryScreen() {
   return (
     <ThemedView style={styles.container}>
-      // Tab content
+        <ThemedText type="title" style={styles.header}>Pantry</ThemedText>
+        <ThemedText style={styles.subtitle}>All your ingredients, at a glance.</ThemedText>
     </ThemedView>
   );
 }
@@ -14,5 +15,14 @@ export default function TabPantryScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    margin: 20,
+    textAlign: 'center',
+  },
+  subtitle: {
+    marginHorizontal: 20,
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
