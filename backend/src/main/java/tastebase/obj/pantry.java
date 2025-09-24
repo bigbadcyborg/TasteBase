ef682488a61e2efb534416f5505355883dc5899d
Aaron Espinoza <rii11@txstate.edu>
2025-09-24 11:45:38 -0500

    EWOK-42 Updated Pantry
    Updated Pantry class to store a list of items, includes appropriate methods to work with. We can change the type of data struct we use later if needed, this is just initially.

--- backend/src/main/java/tastebase/obj/pantry.java
+++ backend/src/main/java/tastebase/obj/pantry.java
@@ -1,19 +1,50 @@
 package tastebase.obj;
 
-public class pantry {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Pantry {
     private int pantryID;
     private String pantryName;
+
+    private final List<Item> pantryItems;
     
-    public pantry(int pantryID, String pantryName) {
+    public Pantry(int pantryID, String pantryName) {
         this.pantryID = pantryID;
         this.pantryName = pantryName;
+
+        // putting this here for now, we are going to need to load items from the database later
+        this.pantryItems = new ArrayList<>(); 
     }
+
+    public boolean addItem(Item newItem) {
+        for (Item i : pantryItems) {
+            if (i.getItemID() == newItem.getItemID()) {
+                return false; // item already exists
+            }
+        }
+        pantryItems.add(newItem);
+        return true; // item added
+    }
+    public boolean removeItem(int itemID) {
+        for (Item i : pantryItems) {
+            if (i.getItemID() == itemID) {
+                pantryItems.remove(i);
+                return true; // item removed
+            }
+        }
+        return false; // item not found
+    }
+    
 
     public int getPantryID() {
         return pantryID;
     }
     public String getPantryName() {
         return pantryName;
+    }
+    public int getItemCount() {
+        return pantryItems.size();
     }
 
     public void setPantryID(int newID) {
@@ -22,4 +53,5 @@ public class pantry {
     public void setName(String newName) {
         this.pantryName = newName;
     }
+
 }
