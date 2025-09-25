fb6224f2beb41f65375abefc8e401c26590a9408
Dylan P <dylan.m.priebe@gmail.com>
2025-09-24 21:29:12 -0500

    EWOK-4 Adjusted API path to /api/

--- backend/src/main/java/tastebase/api/RestAPIController.java
+++ backend/src/main/java/tastebase/api/RestAPIController.java
@@ -1,9 +1,12 @@
 package tastebase.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class RestAPIController {
 
     @GetMapping("/test")
