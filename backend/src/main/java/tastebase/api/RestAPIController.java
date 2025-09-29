28d4c263648ae04a079e9c5477611ad387095705
Dylan <dylan.m.priebe@gmail.com>
2025-09-29 15:04:28 -0500

    EWOK-57 Added initial recipes API

--- backend/src/main/java/tastebase/api/RestAPIController.java
+++ backend/src/main/java/tastebase/api/RestAPIController.java
@@ -1,9 +1,7 @@
 package tastebase.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tastebase.App;
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +10,30 @@ public class RestAPIController {
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("/recipes")
+    public String getRecipes() {
+        return "Use /recipes/random for a random recipe or /recipes/search?ingredients=ing1,ing2 for searching by ingredients, or /recipes/{id} for a specific recipe by ID.";
+    }
+
+    @GetMapping("/recipes/random")
+    public String getRandomRecipe() {
+        return App.getSpoonacularService().getRandomRecipe().toString();
+    }
+
+    @GetMapping("/recipes/search")
+    public String searchRecipes(@RequestParam(required = false) String ingredients) {
+        if (ingredients == null || ingredients.isEmpty()) {
+            return "No ingredients provided, provide as comma-separated list. E.g., ?ingredients=apples,flour,sugar";
+        } else {
+            return App.getSpoonacularService().getRecipes(ingredients).toString();
+        }
+    }
+
+    @GetMapping("/recipes/{id}")
+    public String getRecipeById(@PathVariable int id) {
+        return App.getSpoonacularService().getRecipe(id).toString();
     }
 
 }
